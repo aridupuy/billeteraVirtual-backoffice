b5df6b525b90cfb0a9dd872c62214c713cd44322
mangeluk <mangeluk@grupodigital.com.ar>
2021-07-27 15:34:33 -0300

    Modelo - Blacklist

--- interno/controllers/util_iv.php
+++ interno/controllers/util_iv.php
@@ -4,11 +4,7 @@ class Util_iv extends Controller {
 
     public static $nombre = 'Administracion';public static 
             $modulo = "util_iv";
-    public static $mensaje = "Acá podés ver panel de Administracion.";
-
-    
-
-//    private static $POSICION_7_IMPORTE = 107;
+    public static $mensaje = "Acá podés ver el panel de Administracion.";
 
     public function dispatch($nav, $variables = null) {
         switch ($nav) {
@@ -20,13 +16,10 @@ class Util_iv extends Controller {
                 developer_log(json_encode($variables));
                 $view = $this->home($variables);
                 break;
-            
-
             default:
                 $view = $this->home();
                 break;
         }
-
         return $view;
     }
 
@@ -37,13 +30,8 @@ class Util_iv extends Controller {
             $pagina_a_mostrar = $variables['pagina'];
             unset($variables['pagina']);
         }
-        # Revisar esto, no postear id_marchand
-//        $input = $this->view->createElement('input');
-//        $input->setAttribute('type', 'button');
-//        $input->setAttribute('value', 'Nuevo');
-//        $input->setAttribute('name', 'mod_xxii.create');
         $controller_name = strtolower(get_class($this));
-        $recordset = Usuario::select_usuarios($variables=null);
+        $recordset = Blacklist::select_blacklist($variables=null);
 //        $pager = new Pager($recordset, $pagina_a_mostrar, $controller_name . '.filter');
         $filters = $this->preparar_filtros($variables);
 //        $acciones = array();
@@ -51,61 +39,32 @@ class Util_iv extends Controller {
 //        $acciones[] = array('etiqueta' => 'Editar', 'token' => $controller_name . '.edit', 'id' => 'id_clima');
         // $acciones[] = array('etiqueta' => 'Ver lista de CBU', 'token' => 'Mod_xxiii.filter', 'id' => 'id_clima');
 //        list($array, $labels) = $this->preparar_array($recordset, $pager->desde_registro, $pager->hasta_registro);
-//        foreach ($recordset as $value) {
-            
-//        print_r("<pre>");
-//        print_r($value);
-//        print_r("<pre>");
-//        }
-        # CAPTURAR arrray==false
-//        $labels = array("Apellido", "Nombre", "CUIT/CUIL/DNI", "E-mail", "Estado");
-        
-//        $encabezados = $labels;
-//        $table->cambiar_encabezados($encabezados);
         $form = $this->view->createElement('form');
         $form->setAttribute('id', 'miFormulario');
         $form->setAttribute('class', 'main-content usuarios');
-        $section = $this->view->createElement('section');
-//        $nav = 'create';
-//        $value = 'NUEVO';
-        $encabezado = new Encabezado(self::$modulo, self::$mensaje);
-//        $section->setAttribute('class', 'main-content usuarios');
-        $section->appendChild($this->view->importNode($encabezado->documentElement, true));
-//        $header = $this->view->createElement('header');
-
-//        $titulo = $this->view->createElement('div');
-//        $titulo->setAttribute('class', 'titulo');
-//        $titulo->appendChild($this->view->createTextNode(self::$nombre));
-//        $header->appendChild($titulo);
-//        $section->appendChild($header);
-        $section->appendChild($this->view->importNode($filters->documentElement, true));
-//        $section->appendChild($this->view->importNode($pager->documentElement, true));
-//        $section->appendChild($this->view->importNode($table->documentElement, true));
-//        $section->appendChild($input);
+        $div_80 = $this->view->createElement('div');
+        $total_usr = 'Total: 1500';
+        $encabezado = new Encabezado(self::$modulo, $total_usr);
+        $div_80->setAttribute('class', 'content-80');
+        $div_80->appendChild($this->view->importNode($encabezado->documentElement, true));
         
         $detalle = new Detalle("nombre_completo");
-//        var_dump($recordset);
         $detalle->preparar_arrays($recordset);
-        $acciones[] = (new Accion())->set_campo_id("id_clima")->set_nav("util_i.ver_mas")->set_titulo_nav("Ver más");
-        $acciones[] = (new Toggle())->set_nav_estado("util_i.cambiar_estado")->set_id_estado("id_authstat")->set_campo_id("id_authstat");
-//        var_dump($detalle);
-        $container = new Container($recordset, $detalle, $acciones);
-//                $container->eliminar_columna(0);
-                $container->eliminar_columna(2);
-                $container->eliminar_columna(3);
-                $container->eliminar_columna(4);
-                $container->eliminar_columna(6);
-                $container->eliminar_columna(9);
-                $container->eliminar_columna(10);
-                $container->eliminar_columna(11);
-        $container->crear_desde_recordset();
-        $div_contenido = $this->view->createElement('div');
-       
+        
+        $acciones[] = (new Accion())->set_campo_id("id_clima")->set_nav("util_iv.ver_mas")->set_titulo_nav("Ver más");
+        $acciones[] = (new Toggle())->set_nav_estado("util_iv.cambiar_estado")->set_id_estado("id_authstat")->set_campo_id("id_authstat");
+        
+        $container = new Table($recordset,1, 100);
+        
         $div_cajita = $this->view->createElement('div');
-        $div_cajita->setAttribute('class', 'contenido contenedor-cuentas-bancarias');
+        $div_cajita->setAttribute('class', 'contenedor');
         $div_cajita->appendChild($this->view->importNode($container->documentElement, true));
-        $section->appendChild($div_cajita);
-        $form->appendChild($section);
+        
+        $div_80->appendChild($div_cajita);
+        
+        $form->appendChild($div_80);
+        $form->appendChild($this->view->importNode($filters->documentElement, true));
+        
         $this->view->appendChild($form);
         return $this->view;
     }
@@ -114,7 +73,7 @@ class Util_iv extends Controller {
         $filter = new view();
         if (isset($variables['id']))
             unset($variables['id']);
-        $filter->cargar("views/util_i.filters.html");
+        $filter->cargar("views/util_iv.filters.html");
         $filter->cargar_variables($variables);
         return $filter;
     }
